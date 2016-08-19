4653187a39e69ceca2f3a84f2a1d9ac3b912b6a2
CodeViking13 <92metaller@gmail.com>
2016-08-20 01:32:13 +0300

    simple password generator

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
-x = int(input("Введите кол-во учащихся "))
-if x:
-    i = 1
-    counter = 1
-    print("На первый-второй рассчитайсь:")
-    while i <= x:
-        print(counter)
-        i += 1
-        counter += 1
-        if counter > 2:
-            counter = 1
+import random
+
+# password generator
+def passwordGenerator(count_char=8):
+    arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+           'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'y', 'z']
+    passw = []
+    for i in range(count_char):
+        passw.append(random.choice(arr))
+    return "".join(passw)
+
+newPass = passwordGenerator(15)
+print(newPass)
